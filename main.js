990f3bc1744c471d9ae549d2c298d2358c7eef76
Lachicagladiadora <200379@unsaac.edu.pe>
2023-12-13 21:06:39 -0500

    refactor: logic in js

--- main.js
+++ main.js
@@ -3,51 +3,27 @@ const containElement = document.getElementById("contain");
 const textElement = document.getElementById("phrase");
 const imgElement = document.getElementById("chuck-norris");
 const dialogElement = document.getElementById("dialog");
+const dialogQueueElement = document.getElementById("dialogue-queue");
 
 const chuckNorrisUrl = "https://api.chucknorris.io/jokes/random";
 
 //solicitud GET utilizando la API fetch
-textElement.remove;
-const charge = "...";
-textElement.innerText = charge;
-
 const joke = () => {
+  textElement.remove;
+
+  const charge = "Loading ...";
+
+  textElement.innerText = charge;
+
   fetch(chuckNorrisUrl)
     .then((response) => response.json())
     .then((json) => {
-      // console.log(json);
-      console.log(json.value);
       return !json.value
         ? (textElement.innerText = charge)
         : (textElement.innerText = json.value);
     });
 };
-joke();
 
-const reload = () => {
-  // joke();
-  location.reload();
-};
-
-const messageEnter = (event) => {
-  if (event.keyCode == 13 || event.key === "Enter") {
-    event.preventDefault();
-    imgElement.click() && dialogElement.click();
-  }
-  // joke();
-  reload();
-};
-
-const messageSpace = (e) => {
-  if (e.keyCode == 32 || e.key === "Space") {
-    e.preventDefault();
-    imgElement.click() && dialogElement.click();
-  }
-  // joke();
-  reload();
-};
-
-imgElement.addEventListener("click", reload);
-dialogElement.addEventListener("click", reload);
-window.addEventListener("keyup", messageEnter);
-window.addEventListener("keyup", messageSpace);
+imgElement.addEventListener("click", joke);
+dialogElement.addEventListener("click", joke);
+dialogQueueElement.addEventListener("click", joke);
